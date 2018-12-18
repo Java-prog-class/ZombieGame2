6e998ba99aed1e9c1611cebd9965e7f3093fc345
Josh F <frasjosh808@gotvdsb.ca>
2018-12-18 12:16:09 -0500

    Bullets shoot correctly

--- src/me/zombies/ZombiesMain.java
+++ src/me/zombies/ZombiesMain.java
@@ -204,9 +204,6 @@ public class ZombiesMain implements MouseListener, KeyListener{
 				z.x += z.vx;
 				z.y += z.vy;
 			}
-			
-			z.x-=player.vx;
-			z.y-=player.vy;
 
 			//Detect if zombie and player are in the same location
 			if (z.intersects(player)) {
@@ -360,19 +357,15 @@ public class ZombiesMain implements MouseListener, KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			player.vx=player.speed;
 			movePlayer("right");
 		}
 		if (e.getKeyCode() == KeyEvent.VK_W|| e.getKeyCode() == KeyEvent.VK_UP) {
-			player.vy=-player.speed;
 			movePlayer("up");
 		}
 		if (e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) {
-			player.vx=-player.speed;
 			movePlayer("left");
 		}
 		if (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) {
-			player.vy=player.speed;
 			movePlayer("down");
 		}
 		if (e.getKeyCode() == KeyEvent.VK_Q) {
@@ -396,8 +389,9 @@ public class ZombiesMain implements MouseListener, KeyListener{
 			int mx = e.getX();
 			int my = e.getY();
 			int w = player.currentWeapon;
-			bullets.add(weapons.get(w).shoot(mx,my,player.currentWeapon,player.x,player.y));
-			moveBullets();
+			//bullets.add(new Bullet(0,0));
+			bullets.add(weapons.get(w).shoot(mx,my,player.currentWeapon, panW/2, panH/2));
+			//System.out.println("AHH");
 			
 		}
 		lblAmmo.setText("AMMO: "+ weapons.get(player.currentWeapon).getAmmo());		
